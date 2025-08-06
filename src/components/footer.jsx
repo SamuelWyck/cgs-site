0b95660efd3d4b96078eb8f3793b40b99b32a9d9
Samuel <samawyck@gmail.com>
2025-08-06 12:51:43 -0500

    Create footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,15 @@
+import "../styles/footer.css";
+
+
+
+function Footer() {
+    return (
+        <footer>
+            <p>&copy; CyberGooseSystems</p>
+        </footer>
+    );
+};
+
+
+
+export default Footer;
