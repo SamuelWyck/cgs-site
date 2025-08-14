a77611184a18cedd96c227bfe84fc75db1d1100a
Samuel <samawyck@gmail.com>
2025-08-14 09:52:19 -0500

    Hack out contact page

---
+++ src/components/contactPage.jsx
@@ -0,0 +1,19 @@
+import "../styles/contactPage.css";
+import "../assets/phone.svg";
+
+
+
+function ContactPage() {
+    return (
+        <main className="contact-page">
+            <p className="contact-title">We want to help you</p>
+            <div className="contact-tiles">
+                
+            </div>
+        </main>
+    );
+};
+
+
+
+export default ContactPage;
