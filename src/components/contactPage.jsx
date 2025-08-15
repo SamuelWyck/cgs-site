5f8d963a21ba4bd11f715a1ef14111a3a526bfd2
Samuel <samawyck@gmail.com>
2025-08-15 09:52:53 -0500

    Add dummy data

--- src/components/contactPage.jsx
+++ src/components/contactPage.jsx
@@ -9,17 +9,21 @@ import houseImg from "../assets/home-city.svg";
 function ContactPage() {
     return (
         <main className="contact-page">
-            <p className="contact-title">We want to help you</p>
+            <title>CGS: Contact us</title>
+            <p className="contact-title">Contact us</p>
             <div className="contact-tiles">
-                <div className="contact-tile">
+                <div className="contact-tile phone">
+                    <div className="phone-wrapper">
                     <img src={phoneImg} alt="phone" />
                     <p>414-867-5309</p>
+                    </div>
+                    <p>9am-5pm EST Monday-Friday</p>
                 </div>
-                <div className="contact-tile">
+                <div className="contact-tile email">
                     <img src={emailImg} alt="email" />
                     <p>info@cybergoosesystems.com</p>
                 </div>
-                <div className="contact-tile">
+                <div className="contact-tile address">
                     <img src={houseImg} alt="house" />
                     <p>123 Main Street, Anytown, CA 91234</p>
                 </div>
