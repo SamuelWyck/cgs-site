ad8956096465eec9e1f40a7503660a584f40c065
Samuel <samawyck@gmail.com>
2025-09-17 19:00:04 -0500

    Create service data file

---
+++ src/data/servicesData.js
@@ -0,0 +1,42 @@
+const serviceData = [
+    {
+        title: "This is the main service title",
+        img: "/src/assets/servicesImages/camera.jpg",
+        bulletPoints: [
+            {
+                title: "sub-service title",
+                description: "Describe the service"
+            },
+            {
+                title: "sub-service title",
+                description: "Describe the service"
+            },
+            {
+                title: "sub-service title",
+                description: "Describe the service"
+            }
+        ]
+    },
+    {
+        title: "This is the main service title",
+        img: "/src/assets/servicesImages/camera.jpg",
+        bulletPoints: [
+            {
+                title: "sub-service title",
+                description: "Describe the service this could be very vey long ha konw best to epp it short but it could be very long and the is ok"
+            },
+            {
+                title: "sub-service title",
+                description: "Describe the service"
+            },
+            {
+                title: "sub-service title",
+                description: "Describe the service"
+            }
+        ]
+    }
+];
+
+
+
+export default serviceData;
