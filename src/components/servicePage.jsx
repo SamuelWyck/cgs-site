3b533de6cef04191c52a38423fac5c8ebe281f19
Samuel <samawyck@gmail.com>
2025-09-17 19:10:23 -0500

    Hack out service page

---
+++ src/components/servicePage.jsx
@@ -0,0 +1,35 @@
+import "../styles/servicePage.css";
+import serviceData from "../data/servicesData.js";
+import ServiceCard from "./serviceCard.jsx";
+import { useRef } from "react";
+
+
+
+function ServicePage() {
+    const serviceCards = useRef(getServiceCards(serviceData));
+
+
+    function getServiceCards(services) {
+        const serviceCards = [];
+        for (let service of services) {
+            serviceCards.push(
+                <ServiceCard service={service} key={service.title} />
+            );
+        }
+        return serviceCards;
+    };
+
+
+    return (
+    <main className="service-page">
+        <p className="service-page-title">Services we offer</p>
+        <div className="services">
+            {serviceCards.current}
+        </div>
+    </main>
+    );
+};
+
+
+
+export default ServicePage;
