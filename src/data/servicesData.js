bcb314bc7af493603810f009b005951102cd1a4b
Samuel <samawyck@gmail.com>
2025-09-18 09:58:13 -0500

    Add more dummy data

--- src/data/servicesData.js
+++ src/data/servicesData.js
@@ -34,6 +34,24 @@ const serviceData = [
                 description: "Describe the service"
             }
         ]
+    },
+    {
+        title: "This is the main service title",
+        img: "/src/assets/servicesImages/camera.jpg",
+        bulletPoints: [
+            {
+                title: "sub-service title",
+                description: "Describe the service this could be very vey long ha konw best to epp it short but it could be very long and the is ok"
+            },
+            {
+                title: "sub-service title",
+                description: "Describe the service"
+            },
+            {
+                title: "sub-service title",
+                description: "Describe the service"
+            }
+        ]
     }
 ];
 
