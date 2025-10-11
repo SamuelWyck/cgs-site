4639ea08fb3e89583b06a383a6baef1a9e0da602
Samuel <samawyck@gmail.com>
2025-10-10 19:28:20 -0500

    Shorten quote

--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -31,10 +31,9 @@ function HomePage() {
         <section className="quote-section">
             <div className="quote-wrapper">
                 <p className="quote">
-                    Cyber  Goose Systems LLC is a a cutting edge information technology and cybersecurity service provider. 
+                    Cyber Goose Systems LLC is a a cutting edge information technology and cybersecurity service provider. 
                     Founded in 2025, our mission is to deliver robust IT solutions and Comprehensive Cybersecurity services to 
-                    businesses of all sizes. We are committed to helping our clients navigate the complexities of the Digital landscape, 
-                    ensuring their systems are secure, efficient and compliant with the industry standards.
+                    businesses of all sizes.
                 </p>
             </div>
         </section>
